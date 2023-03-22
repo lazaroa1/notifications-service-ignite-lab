69f6320c2521682e11df3f2e2e4735e85627a133
lazaroa1 <lazaroalves36@gmail.com>
2023-03-22 15:25:44 -0300

    creations firsts tests

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,3 +1,4 @@
+import { Replace } from 'src/helpers/Replace';
 import { Content } from './content';
 
 export interface NotificationProps {
@@ -11,8 +12,8 @@ export interface NotificationProps {
 export class Notifications {
   private props: NotificationProps;
 
-  constructor(props: NotificationProps) {
-    this.props = props;
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = { ...props, createdAt: props.createdAt ?? new Date() };
   }
 
   public set recipientId(recipientId: string) {
